14cf8093c2bd1d1038b7d80467217e692ac10d5d
mmdaminah <en.mmdamin@gmail.com>
2021-12-01 11:21:25 +0330

    ipfs test for server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,29 @@
 const express = require('express')
+const multer = require('multer')
+
 const app = express()
+const storage = multer.diskStorage(
+  {
+    destination:(req,file,cb)=>{
+      cb(null,'uploads/')  
+    },
+    filename:(req,file,cb)=>{
+      cb(null,req.body.hash)
+    }
+  },
+  )
+const upload = multer({storage})
 const port = 5000
+
 app.use(express.json())
-let estimatedTime = 13;
-app.get('/', (req, res) => {
-  res.send("hello")
+
+app.post('/upload',upload.single('file'),(req,res)=>{
+  console.log(req.body);
+  console.log(req.file);
+  res.json({ message: "Successfully uploaded files" });
 })
-app.get('/estimatedTime',(req,res)=>{
-    res.status(200).send({estimatedTime});
-})
-app.post('/estimatedTime',(req,res)=>{
-    const body = req.body;
-    console.log(body)
-    estimatedTime = (estimatedTime + body.time)/2;
-    console.log(estimatedTime)
-    res.status(200).send("ok")
-})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at ${port}`)
+  console.log(`server listening at ${port}`)
 })
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,27 +1,46 @@
 import React, { useState } from 'react'
 import { create } from 'ipfs-http-client'
 import { useWeb3React } from "@web3-react/core";
-// const client = create('https://ipfs.infura.io:5001/api/v0')
+import axios from 'axios';
+import { Form } from 'react-bootstrap';
 const contractAddress = "0x165B4cEAd9377079A05Be08DFe0881CF79879C0C"
 const contractABI = [{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_symbol","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_account","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"print","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_account","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"redeem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-const client = create('/ip4/127.0.0.1/tcp/5001')
+// const client = create('/ip4/127.0.0.1/tcp/5001')
+// const client = create('https://ipfs.infura.io:5001/api/v0')
+const client = create('https://asiapal.org/api/v0')
 const HomePage = () => {
     const { library, active, account } = useWeb3React()
     const [file,setFile] = useState()
     const [hash,setHash] = useState()
     const sendFile = async ()=>{
         try{
-            let width, height;
             const size = file.size
+            console.log(file.name.split('.')[1])
             let img = new Image()
             img.src = window.URL.createObjectURL(file)
             img.onload = () => {
                console.log(img.width,img.height) 
             }
+            
+            //********uploading ipfs******
             const added = await client.add(file)
             console.log(added)
-            // const fileUrl = `https://ipfs.infura.io/ipfs/${added.path}`
-            setHash(added.path)
+            const fileUrl = `https://ipfs.infura.io/ipfs/${added.path}`
+            // const fileUrl = `https://asiapal.org/ipfs/${added.path}`
+            setHash(fileUrl)
+            //********uploading ipfs******
+            //*****************************//
+            //********uploading locally******
+            const formData = new FormData()
+            // formData.append('hash',added.path)
+            formData.append('hash',"sdfadded.path")
+            formData.append("file",file)
+            axios.post('http://files.lott.link/upload',formData,{
+              headers:{
+                'Content-Type':'multipart/form-data'
+              }
+            }).then(()=>console.log('uploaded to server'))
+            //********uploading locally******   
         }
         catch(error){
             console.log("error occured whilte uploading the file!",error)
