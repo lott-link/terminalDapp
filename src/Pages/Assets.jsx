cf95a6d054156c6549f763884f7cf6320b7ecfaa
mmdaminah <en.mmdamin@gmail.com>
2022-01-16 12:31:10 +0330

    image lazy loading added to assets

--- src/Pages/Assets.jsx
+++ src/Pages/Assets.jsx
@@ -4,6 +4,10 @@ import { useWeb3React } from "@web3-react/core";
 import { context } from '../App'
 import Button from '../Components/styled/Button';
 import Spinner from 'react-bootstrap/Spinner'
+
+import 'react-loading-skeleton/dist/skeleton.css'
+import ContentLoader from '../../node_modules/react-loading-skeleton'
+
 const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"privateFileHash","outputs":[{"internalType":"string","name":"_privateFileHash","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"privateInfo","outputs":[{"internalType":"string","name":"_privateInfo","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"publicFileHash","outputs":[{"internalType":"string","name":"_publicFileHash","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"publicInfo","outputs":[{"internalType":"string","name":"_publicInfo","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"string","name":"uri","type":"string"},{"internalType":"string","name":"publicInfo","type":"string"},{"internalType":"string","name":"privateInfo","type":"string"},{"internalType":"string","name":"publicFileHash","type":"string"},{"internalType":"string","name":"privateFileHash","type":"string"}],"name":"safeMint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 const Assets = () => {
     const data = useContext(context);
@@ -123,7 +127,8 @@ const Assets = () => {
                 <Spinner style={{width:"5rem",height:"5rem"}} animation="grow" variant="light" />
                 <Spinner className='mx-2' style={{width:"5rem",height:"5rem"}} animation="grow" variant="light" />
                 <Spinner style={{width:"5rem",height:"5rem"}} animation="grow" variant="light" />
-            </div>}
+            </div>
+            }
             </div>
             {show && 
                 <div className="w-25 d-flex flex-column align-items-center"
@@ -143,9 +148,14 @@ export default Assets
 
 
 const NFTCard = ({description="there is no description",image,NFTName="no nmae"})=>{
+    const [loading,setLoading] = useState(true)
+    const handleLoad = ()=> setLoading(false)
     return (
         <div className='w-25' style={{minHeight:"12rem"}}>
-            <div className='w-100 h-50 bg-light'><img className='w-100 h-100' style={{objectFit:"contain"}} src={image} alt="" /></div>
+            <div className='w-100 h-50 bg-light'>
+                 <img className='w-100 h-100' onLoad={handleLoad} style={{objectFit:"contain",display:loading?"none":"initial"}} src={image} alt="" />
+                {loading && <LazyImage className='w-100 h-100' />}
+            </div>
             <div className='w-100 bg-white text-dark p-4'>
                 <div><strong>{NFTName}</strong></div>
                 <div>{description}</div>
@@ -163,3 +173,23 @@ const NFTCard = ({description="there is no description",image,NFTName="no nmae"}
         </div>
     )
 }
+
+const LazyImage = props => {
+    return (
+      <ContentLoader 
+        speed={2}
+        viewBox="0 0 400 160"
+        backgroundColor="#d9d9d9"
+        foregroundColor="#ededed"
+        {...props}
+      >
+        <rect x="50" y="6" rx="4" ry="4" width="343" height="38" />
+        <rect x="8" y="6" rx="4" ry="4" width="35" height="38" />
+        <rect x="50" y="55" rx="4" ry="4" width="343" height="38" />
+        <rect x="8" y="55" rx="4" ry="4" width="35" height="38" />
+        <rect x="50" y="104" rx="4" ry="4" width="343" height="38" />
+        <rect x="8" y="104" rx="4" ry="4" width="35" height="38" />
+      </ContentLoader>
+    )
+  }
+  
