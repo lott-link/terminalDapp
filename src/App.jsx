2ca13358e691887f5d458754950808262b8ecfd9
mmdaminah <en.mmdamin@gmail.com>
2022-02-26 12:07:28 +0330

    Change browserRouter to hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react'; 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import mainRoutes from "./Routes/mainRoutes";
 import MyNavbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
